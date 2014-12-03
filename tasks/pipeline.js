c21f68a30b86ca3d10cfafbeb30e90f3dd705e65
Ubuntu <ubuntu@badgerworks.pwnz.org>
2014-12-03 04:36:33 +0000

    [styles] cleaned nav bar

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -27,7 +27,7 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  'js/dependencies/jquery.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
